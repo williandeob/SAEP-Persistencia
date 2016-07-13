70b19685f80d1a7fe861bb22ae24dd268a5d2c1d
willian <williandeob@gmail.com>
2016-07-12 21:32:40 -0300

    adicionar nota

--- SaepPersistencia/src/main/java/br/ufg/inf/es/saep/sandbox/persistencia/ParecerRepositoryImp.java
+++ SaepPersistencia/src/main/java/br/ufg/inf/es/saep/sandbox/persistencia/ParecerRepositoryImp.java
@@ -1,16 +1,52 @@
 package br.ufg.inf.es.saep.sandbox.persistencia;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.text.InternationalFormatter;
+
 import br.ufg.inf.es.saep.sandbox.dominio.Avaliavel;
+import br.ufg.inf.es.saep.sandbox.dominio.IdentificadorDesconhecido;
 import br.ufg.inf.es.saep.sandbox.dominio.Nota;
 import br.ufg.inf.es.saep.sandbox.dominio.Parecer;
 import br.ufg.inf.es.saep.sandbox.dominio.ParecerRepository;
 import br.ufg.inf.es.saep.sandbox.dominio.Radoc;
+import br.ufg.inf.es.saep.sandbox.util.UtilJsonPersistencia;
 
 public class ParecerRepositoryImp implements ParecerRepository{
-
+	public static String repositoryParecer = "src/main/resources/br/ufg/inf/es/saep/sandbox/persistencia/Parecer/";
+	
 	@Override
 	public void adicionaNota(String id, Nota nota) {
-		// TODO Auto-generated method stub
+		File file = new File(repositoryParecer.concat(id).concat(".json"));
+		if(!file.exists()){
+			throw new IdentificadorDesconhecido("id");
+		}else{
+			try {
+				Parecer parecer = (Parecer) UtilJsonPersistencia.recuperarJson(file.getAbsolutePath(), Parecer.class);
+				List<Nota> notas = parecer.getNotas();
+				Iterator<Nota> inotas = notas.iterator();
+				boolean novaNota = true;
+				
+				while(inotas.hasNext()){
+					Nota notaItem = inotas.next();
+					if(notaItem.getItemOriginal().equals(nota.getItemOriginal())){
+						novaNota = false;	
+					}
+				}
+				
+				if(novaNota){
+					notas.add(nota);
+					UtilJsonPersistencia.persistirJson(parecer, file.getAbsolutePath());
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
